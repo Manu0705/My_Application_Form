f4e002bd22163c330244cd20dd9b0e83a897f996
Manu0705 <manoja.07@outlook.com>
2025-10-13 15:52:20 +0530

    Updated app.py with latest changes

--- app.py
+++ app.py
@@ -25,10 +25,11 @@ def submit():
     qualification = request.form['qualification']
     email = request.form['email']
     phone = request.form['phone']
+    location = request.form['location']
     message = request.form['message']
 
-    sql = "INSERT INTO applications (fullname, fathername, mothername, qualification, email, phone, message) VALUES (%s, %s,%s, %s, %s, %s, %s)"
-    values = (fullname, fathername, mothername, qualification, email, phone, message)
+    sql = "INSERT INTO applications (fullname, fathername, mothername, qualification, email, phone, location, message) VALUES (%s, %s,%s, %s, %s, %s, %s, %s)"
+    values = (fullname, fathername, mothername, qualification, email, phone, location, message)
     cursor.execute(sql, values)
     db.commit()
 
